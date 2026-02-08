ead77daa1be0cede407e18543ffcdce8dc260ef0
Hasnat ali <167166095+shahzaib989@users.noreply.github.com>
2026-02-08 17:51:25 +0500

    banner--v2

--- assets/banner-v2.js
+++ assets/banner-v2.js
@@ -1,17 +1,18 @@
 
 var drawerOpenbtn= document.querySelector(".open-drawer");
 var drawerClosebtn=document.querySelector(".close-drawer");
-
+var drawer=document.querySelector(".drawer--main--banner");
 drawerOpenbtn.addEventListener("click", function(){
   drawerClosebtn.style.display="block";
   drawerOpenbtn.style.display="none";
-
+  drawer.classList.add("active");
 
 })
 
 drawerClosebtn.addEventListener("click", function(){
   drawerClosebtn.style.display="none";
   drawerOpenbtn.style.display="block";
+  drawer.classList.remove("active");
 
 })
 
