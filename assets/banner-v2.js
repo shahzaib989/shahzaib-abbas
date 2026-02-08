52c1bd7f19e27f17c0c9e5cd46957e333d29eac9
Hasnat ali <167166095+shahzaib989@users.noreply.github.com>
2026-02-08 17:47:20 +0500

    drawer-javascript

--- assets/banner-v2.js
+++ assets/banner-v2.js
@@ -1,1 +1,17 @@
-console.log("thisisthis")
+
+var drawerOpenbtn= document.querySelector("open-drawer");
+var drawerClosebtn=document.querySelector(".close-drawer");
+
+drawerOpenbtn.addEventListener("click", function(){
+  drawerClosebtn.style.display="block";
+  drawerOpenbtn.style.display="none";
+
+
+})
+
+drawerClosebtn.addEventListener("click", function(){
+  drawerClosebtn.style.display="none";
+  drawerOpenbtn.style.display="block";
+
+})
+
