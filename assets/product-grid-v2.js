79e7776f200d60fb956bee05a9fa7e92caa61e2c
Hasnat ali <167166095+shahzaib989@users.noreply.github.com>
2026-02-09 13:20:26 +0500

    product-grid-popup

--- assets/product-grid-v2.js
+++ assets/product-grid-v2.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
    var allBtnsPopup=document.querySelectorAll(".popup__btn-main");
+
+
+
+
+
+
+
+   
    allBtnsPopup.addEventListener("click", function(){
-    
+
    })
 })
