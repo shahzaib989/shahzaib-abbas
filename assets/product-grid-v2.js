f60ef8a9e0f202f4c284d8414c2d891b7ea079f4
Hasnat ali <167166095+shahzaib989@users.noreply.github.com>
2026-02-09 13:44:51 +0500

    Update product-grid-v2.js

--- assets/product-grid-v2.js
+++ assets/product-grid-v2.js
@@ -3,17 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
   var allBtnsPopup = document.querySelectorAll(".popup__btn-main");
   var popupMain = document.querySelector("#popup--info");
   var closeBtn = document.querySelector(".popupClosebtn");
+  var bodyMain=document.querySelector("body");
 
 
   function openPopup() {
     if (popupMain) {
       popupMain.classList.add("active");
+      bodyMain.style.overflow="hidden";
     }
   }
 
   function closePopup() {
     if (popupMain) {
       popupMain.classList.remove("active");
+       bodyMain.style.overflow="unset";
     }
   }
 
